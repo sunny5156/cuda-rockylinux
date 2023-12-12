67036b98e1b7ee866d2e82182ab75ea203379d4a
sunny5156 <sunny5156@qq.com>
2023-12-12 16:39:15 +0800

    jupyterhub==4.0.1  dockerspawner==13.0.0 oauthenticator==16.2.1  docker==6.1.1

--- jupyterlab-install.sh
+++ jupyterlab-install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:684a591831b38f4a3bf7b10f9e6e37241ed07cfd3d329e13720202cb030006ac
-size 3660
+oid sha256:80a49a65a76ab612aead2007c69a40c1aade257345a59de3290c47ba7efbe917
+size 3691
